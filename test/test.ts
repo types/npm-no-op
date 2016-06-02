c2f0f7aeb0d2cf93f48239fe44c0f6281fab0e67
Homa Wong <homawong@gmail.com>
2016-06-02 11:42:08 -0700

    Improve teset

--- test/test.ts
+++ test/test.ts
@@ -4,10 +4,11 @@ import noop = require('no-op');
 
 test('no-op', (t) => {
   class A {
-    fn: (a: string, b: number) => string[]
+    fn: (a: string, b: number) => string[];
     constructor() {
       this.fn = noop;
     }
   }
+  t.pass('noop can assigned to (a: string, b: number) => string[]');
   t.end();
 });
